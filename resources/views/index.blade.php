c98faecbfe2a413e2fa812da334f95fa0b3d15d6
unknown <rafli.ran3103@gmail.com>
2023-02-15 15:09:24 +0700

    @Rafli: coba lagi gan

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@section('content')
+<div class="container" id="container">
+    <div class="wrapper">
+        <h3 class="label">
+            Halo kawan
+        </h3>
+    </div>
+</div>
+@endsection
